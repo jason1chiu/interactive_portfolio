f2b3fae3dada24e25c9125487c1ac37ec8ac7d08
jason1chiu <119711904+jason1chiu@users.noreply.github.com>
2023-07-29 19:58:46 -0400

    edit package.json

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -2,15 +2,11 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const skillRoutes = require('./skillRoutes');
 const projectRoutes = require('./projectRoutes');
-const blogPostRoutes = require('./blogPostRoutes');
-const testimonialRoutes = require('./testimonialRoutes');
 const contactMessageRoutes = require('./contactMessageRoutes');
 
 router.use('/users', userRoutes);
 router.use('/skills', skillRoutes);
 router.use('/projects', projectRoutes);
-router.use('/blogposts', blogPostRoutes);
-router.use('/testimonials', testimonialRoutes);
 router.use('/contactmessages', contactMessageRoutes);
 
 module.exports = router;
