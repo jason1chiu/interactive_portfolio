e7d8baeff9c32176423b066a5ed06453493ca87e
jason1chiu <119711904+jason1chiu@users.noreply.github.com>
2023-08-06 11:17:25 -0400

    change skills item switchs

--- client/src/views/Home/Skills/Skills.js
+++ client/src/views/Home/Skills/Skills.js
@@ -13,12 +13,12 @@ const responsive = {
   desktop: {
     breakpoint: { max: 3000, min: 1024 },
     items: 4,
-    slidesToSlide: 1,
+    slidesToSlide: 4,
   },
   tablet: {
     breakpoint: { max: 1024, min: 464 },
     items: 3,
-    slidesToSlide: 1,
+    slidesToSlide: 3,
   },
   mobile: {
     breakpoint: { max: 464, min: 0 },
